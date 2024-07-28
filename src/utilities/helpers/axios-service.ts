5bd0506db251c3acb2d6fc4f33dae51cd7845c1c
heysaqlain01 <saqlain@freeads.co.uk>
2024-07-28 18:21:02 +0500

    fix api issue

--- src/utilities/helpers/axios-service.ts
+++ src/utilities/helpers/axios-service.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
-import Config from 'react-native-config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Platform} from 'react-native';
+
+const baseURL =
+  Platform.OS === 'ios' ? 'http://127.0.0.1:4000/' : 'http://10.0.2.2:4000/';
 
 const axiosService = axios.create({
-  baseURL: `http://127.0.0.1:4000/`,
-
+  baseURL,
   headers: {
     'Content-Type': 'multipart/form-data',
   },
@@ -12,26 +14,31 @@ const axiosService = axios.create({
 
 axiosService.interceptors.request.use(
   async config => {
-    const userAuthToken = await AsyncStorage.getItem('@USER');
-    if (config?.headers?.Authorization) {
-      config.headers['Authorization'] = config?.headers?.Authorization;
-    } else if (userAuthToken) {
-      config.headers['Authorization'] = ` Bearer ${userAuthToken}`;
+    try {
+      const userAuthToken = await AsyncStorage.getItem('@USER');
+      if (config?.headers?.Authorization) {
+        config.headers['Authorization'] = config?.headers?.Authorization;
+      } else if (userAuthToken) {
+        config.headers['Authorization'] = `Bearer ${userAuthToken}`;
+      }
+      return config;
+    } catch (error) {
+      console.error('Error in request interceptor:', error);
+      return Promise.reject(error);
     }
-
-    return config;
   },
   error => {
+    console.error('Request error:', error);
     return Promise.reject(error);
   },
 );
 
 axiosService.interceptors.response.use(
-  res => {
-    return res;
+  response => {
+    return response;
   },
   async error => {
-    console.log('xx- error', error.response);
+    console.error('Response error:', error);
     if (error.response && error.response.status === 503) {
       // Navigate to maintenance screen
       // Call your navigation function to navigate to maintenance screen
